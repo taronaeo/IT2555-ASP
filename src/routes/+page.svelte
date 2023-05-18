0d9c8448dfdde1efb6f23cdec15b44abee2db9cd
Junjie <J_unj_ie@outlook.com>
2023-05-18 21:19:43 +0800

    feat: add my name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
 
 <section class="mx-80">
 	<h1 class="text-4xl font-bold">Welcome to IT2555 - App Sec Project</h1>
+	
+	<H2>Junjie was here</H2>
 
 	<p>To check if SSR is ready, navigate to <a href="/api" class="text-blue-600 underline">/api</a></p>
 </section>
