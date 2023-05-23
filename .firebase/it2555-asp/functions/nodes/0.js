024c2ec1995809f8df6b7a4a8c2e51092617352f
Aaron Teo <aaron.teo@riv-alumni.com>
2023-05-23 14:26:17 +0800

    chore: remove ignored files

--- .firebase/it2555-asp/functions/nodes/0.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.d0c91b3b.js","_app/immutable/chunks/index.81aa6c8c.js"];
-export const stylesheets = ["_app/immutable/assets/0.4ecc3c96.css"];
-export const fonts = ["_app/immutable/assets/dm-sans-latin-ext-400-normal.83c62f43.woff2","_app/immutable/assets/dm-sans-all-400-normal.52bacf2c.woff","_app/immutable/assets/dm-sans-latin-400-normal.7164a212.woff2"];
--- .firebase/it2555-asp/functions/nodes/2.js
+++
@@ -1,9 +0,0 @@
-import * as universal from '../entries/pages/_page.ts.js';
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export { universal };
-export const universal_id = "src/routes/+page.ts";
-export const imports = ["_app/immutable/nodes/2.f4d512a7.js","_app/immutable/chunks/index.81aa6c8c.js"];
-export const stylesheets = [];
-export const fonts = [];
