974c7a2efc8e59b0403aa883b3c5dc67bc0386c6
Aaron Teo <aaron.teo@riv-alumni.com>
2023-06-12 02:48:34 +0800

    wip: user create writes to firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,3 +17,50 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+import * as logger from 'firebase-functions/logger';
+
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import {
+  BlockingOptions,
+  beforeUserCreated,
+} from 'firebase-functions/v2/identity';
+
+const runtime: BlockingOptions = {
+  region: 'asia-southeast1',
+  maxInstances: 1,
+};
+
+export const onBeforeUserCreated = beforeUserCreated(runtime, async (e) => {
+  initializeApp();
+  const eventData = e.data;
+  const user = {
+    uid: eventData.uid,
+    disabled: eventData.disabled,
+    display_name: eventData.displayName || 'Dr. Receipts User',
+    email: eventData.email!,
+    email_verified: eventData.emailVerified,
+    phone_number: eventData.phoneNumber || null,
+    photo_url:
+      eventData.photoURL ||
+      'https://api.dicebear.com/6.x/big-smile/svg?seed=Dr.%20Receipts%20User',
+    tenant_id: eventData.tenantId || '[DEFAULT]',
+    custom_claims: eventData.customClaims || {},
+  };
+
+  const db = getFirestore();
+
+  logger.info('onBeforeUserCreated:userData', user);
+
+  try {
+    await db.collection('users').doc(user.uid).set(user, { merge: true });
+  } catch (error) {
+    logger.error('onBeforeUserCreated:error', error);
+    throw error;
+  }
+
+  return {
+    displayName: eventData.displayName || 'Dr. Receipts User',
+  };
+});
