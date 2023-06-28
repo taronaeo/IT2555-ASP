58bda7cd912572260af101343155b4bd25bf867e
Aaron Teo <aaron.teo@riv-alumni.com>
2023-06-29 02:50:58 +0800

    fix: oversized loading animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
   <div
     class="z-50 fixed top-0 w-screen h-screen
             flex flex-col justify-center items-center">
-    <AniIconLoading fill="#059669" />
+    <AniIconLoading fill="#059669" class='w-20 h-20 aspect-1' />
   </div>
 {:else}
   <slot />
