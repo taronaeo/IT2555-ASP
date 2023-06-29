0fcd0893c87c45b2ff96017569d9bf35624a5f83
Aaron Teo <aaron.teo@riv-alumni.com>
2023-06-30 03:01:45 +0800

    refactor: update sign out redirect

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,36 +1,38 @@
 <script>
-	import { goto } from '$app/navigation'
-	import { auth } from '$lib/firebase'
-	import { authStore } from '$lib/stores'
+  import { goto } from '$app/navigation';
+  import { authStore } from '$lib/stores';
 
-	import { signOut } from 'firebase/auth'
+  import { signOut } from '$lib/firebase/auth';
 
-	async function onSignOut() {
-		await signOut(auth);
-		await goto('/account/login');
-	}
+  async function onSignOut() {
+    await signOut();
+    await goto('/account/signin');
+  }
 </script>
 
 <svelte:head>
-	<title>IT2555</title>
+  <title>World of Digital Receipts | Dr. Receipts</title>
 </svelte:head>
 
 <section>
-	<pre>{JSON.stringify($authStore, null, 2)}</pre>
+  <pre>{JSON.stringify($authStore, null, 2)}</pre>
 
-	{#if $authStore !== null}
-		<button on:click={onSignOut}>Logout</button>
-	{/if}
+  {#if $authStore !== null}
+    <button on:click={onSignOut}>Logout</button>
+  {/if}
 
-	<h1 class="text-4xl font-bold">Welcome to IT2555 - App Sec Project</h1>
+  <h1 class="text-4xl font-bold">Welcome to IT2555 - App Sec Project</h1>
 
-	<h2>Junjie was here</h2>
+  <h2>Junjie was here</h2>
 
-	<h3>deziduck was here</h3>
+  <h3>deziduck was here</h3>
 
-	<h3> Trav was here </h3>
+  <h3> Trav was here </h3>
 
-	<h3>Aaron was here</h3>
+  <h3>Aaron was here</h3>
 
-	<p>To check if SSR is ready, navigate to <a href="/api" class="text-blue-600 underline">/api</a></p>
+  <p>
+    To check if SSR is ready, navigate to
+    <a href="/api" class="text-blue-600 underline">/api</a>
+  </p>
 </section>
