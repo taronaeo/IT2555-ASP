1a434434ead7cdd304d88433a144d5d39fb5ba95
Aaron Teo <aaron.teo@riv-alumni.com>
2023-07-02 14:39:10 +0800

    refactor: remove margin bottom from Alert

--- src/lib/components/Alert.svelte
+++ src/lib/components/Alert.svelte
@@ -2,9 +2,7 @@
   import { IconInfo } from '$lib/icons';
 </script>
 
-<div
-  class="p-4 mb-4 flex flex-row bg-red-50 text-red-800 rounded-lg"
-  role="alert">
+<div class="p-4 flex flex-row bg-red-50 text-red-800 rounded-lg" role="alert">
   <IconInfo class="w-5 h-5" />
   <div class="ml-2 text-sm font-medium">
     <slot />
