ede841c78e3bdd4c0d7ea9d615a27e86d037b4ca
Aaron Teo <aaron.teo@riv-alumni.com>
2023-07-07 00:25:10 +0800

    feat: preload auth state

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,18 @@
 <script>
   import '../app.css';
-  import { authStore } from '$lib/stores'
-  import { AniIconLoading } from '$lib/icons'
+
+  import { authStore } from '$lib/stores';
+  import { AniIconLoading } from '$lib/icons';
+
+  // Preload auth state
+  $authStore;
 </script>
 
 {#if $authStore === undefined}
   <div
     class="z-50 fixed top-0 w-screen h-screen
             flex flex-col justify-center items-center">
-    <AniIconLoading fill="#059669" class='w-20 h-20 aspect-1' />
+    <AniIconLoading fill="#059669" class="w-20 h-20 aspect-1" />
   </div>
 {:else}
   <slot />
