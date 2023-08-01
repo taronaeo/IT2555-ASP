8a4a8e833934c4daee88e84fbdc7404b3385c098
Aaron Teo <aaron.teo@riv-alumni.com>
2023-08-01 21:47:51 +0800

    fix: navbar signin redirect

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -11,44 +11,53 @@
   class="z-50 fixed top-0 w-full
           border-b-4 border-emerald-700
           bg-gradient-to-r from-emerald-600 to-[#008080]">
-  <div class="h-16 mx-auto container
+  <div
+    class="h-16 mx-auto container
               flex flex-row flex-wrap justify-start">
     <!-- Dr. Receipt Logo -->
     <a href="/" class="px-6 py-2 h-full">
-      <Logo class='h-full' gradient_start='#fff' gradient_stop='#fff' />
+      <Logo class="h-full" gradient_start="#fff" gradient_stop="#fff" />
     </a>
 
     <div class="ml-auto flex flex-row md:order-2">
       <!-- Navbar CTA Button -->
       <a
-        href={!$authStore ? '/account/login' : '/account/profile'}
+        href={!$authStore ? '/account/signin' : '/account/profile'}
         class="p-4 h-full inline-flex items-center
                 text-lg text-white text-center font-medium
                 hover:text-emerald-200 focus:outline-none">
-          {!$authStore ? 'Login' : 'Profile'}
-        </a>
+        {!$authStore ? 'Login' : 'Profile'}
+      </a>
 
       <!-- Navbar Mobile Menu Button -->
       <button
-        on:click={() => isDropdownActive = !isDropdownActive}
+        on:click={() => (isDropdownActive = !isDropdownActive)}
         class="p-4 w-full h-full inline-flex items-center justify-center
                 md:hidden focus:outline-none">
-        <img src={!isDropdownActive ? IconMenu : IconClose} alt="Open Menu" class="m-1 w-6 h-6">
+        <img
+          src={!isDropdownActive ? IconMenu : IconClose}
+          alt="Open Menu"
+          class="m-1 w-6 h-6" />
       </button>
     </div>
 
     <!-- Middle Menu Links -->
-    <div class="{isDropdownActive ? 'block' : 'hidden md:flex'}
+    <div
+      class="{isDropdownActive ? 'block' : 'hidden md:flex'}
                 py-4 z-20 w-full min-h-full md:h-full md:p-0
                 flex flex-col md:flex-row md:items-center
                 bg-gradient-to-r from-emerald-600 to-[#008080]
                 md:w-auto md:order-1 md:from-transparent md:to-transparent">
-      <a href="/" class="px-6 py-2 h-full text-lg text-white font-medium
+      <a
+        href="/"
+        class="px-6 py-2 h-full text-lg text-white font-medium
                           md:py-5 md:text-white md:hover:bg-emerald-700">
         Who we are
       </a>
 
-      <a href="/" class="px-6 py-2 h-full text-lg text-white font-medium
+      <a
+        href="/"
+        class="px-6 py-2 h-full text-lg text-white font-medium
                           md:py-5 md:text-white md:hover:bg-emerald-700">
         Tools and services
       </a>
