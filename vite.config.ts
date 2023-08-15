f4f2addf55f4476e66979fcbbd01e468181312c3
Aaron Teo <aaron.teo@riv-alumni.com>
2023-08-15 19:38:49 +0800

    fix: disappearing login button on navbar

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: '0.0.0.0',
-		port: 3000,
-		strictPort: true
-	}
+  plugins: [sveltekit()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+  },
 });
