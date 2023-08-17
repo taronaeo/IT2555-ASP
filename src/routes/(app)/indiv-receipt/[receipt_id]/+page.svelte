8715b7f9dae77c0b73eb9448f31b2a010673bccc
Dexter Teo <dexter.teo1320@gmail.com>
2023-08-17 08:07:46 +0800

    feat: added authguard to receipt views

--- src/routes/(app)/indiv-receipt/[receipt_id]/+page.svelte
+++ src/routes/(app)/indiv-receipt/[receipt_id]/+page.svelte
@@ -8,10 +8,8 @@
   import dayjs from 'dayjs';
   import { getHttpsCallable } from '$lib/firebase/functions';
   import { AniIconLoading } from '$lib/icons';
+  import { AuthGuard } from '$lib/components';
 
-  $: if ($authStore === null) {
-    goto('/account/signin');
-  }
   const userUid = $authStore?.uid;
   const storage = getStorage();
 
@@ -24,8 +22,7 @@
     const onReceiptViewCallable = getHttpsCallable('onReceiptViewCallable');
     const receiptData = await onReceiptViewCallable(current_receipt_id).catch(
       (err) => {
-        console.log(err);
-        goto('/');
+        console.error(err);
       }
     );
 
@@ -39,15 +36,6 @@
     return [data, date, time];
   }
   let promise = docSnap();
-
-  async function assignReceipt() {
-    const receiptRef = doc(firestore, 'receipts', current_receipt_id);
-    updateDoc(receiptRef, {
-      userUid: userUid,
-    }).then(() => {
-      window.location.reload();
-    });
-  }
 
   async function downloadReceipt() {
     downloadLoading = true;
@@ -95,102 +83,103 @@
   }
 </script>
 
-<div
-  class:hidden={!downloadLoading}
-  class="flex fixed justify-center items-center inset-0 w-full bg-black/50">
+<AuthGuard>
   <div
-    class="text-center text-md py-2 w-10/12 md:w-2/3 lg:w-1/3 xl:w-1/3 px-4 font-bold bg-white border-2">
-    Receipt PDF is Generating<br />
-    Wait a Moment...
-    <AniIconLoading class="m-auto my-4" fill="#059669" />
+    class:hidden={!downloadLoading}
+    class="flex fixed justify-center items-center inset-0 w-full bg-black/50">
+    <div
+      class="text-center text-md py-2 w-10/12 md:w-2/3 lg:w-1/3 xl:w-1/3 px-4 font-bold bg-white border-2">
+      Receipt PDF is Generating<br />
+      Wait a Moment...
+      <AniIconLoading class="m-auto my-4" fill="#059669" />
+    </div>
   </div>
-</div>
 
-{#await promise}
-  <div class="flex justify-center pt-64">
-    <AniIconLoading class=" my-4" fill="#059669" />
-  </div>
-{:then data}
-  <div class="md:grid md:grid-cols-2 my-4">
-    <div class="flex justify-center mb-12">
-      <div
-        class="w-2/3 py-12 px-4 border-x-2 border-b-2 border-t-4 mx-6 border-t-emerald-600 shadow-lg">
-        <div class="text-center">
-          <hr class=" border-black border-[1px] border-dashed mb-6 mx-10" />
+  {#await promise}
+    <div class="flex justify-center pt-64">
+      <AniIconLoading class=" my-4" fill="#059669" />
+    </div>
+  {:then data}
+    <div class="md:grid md:grid-cols-2 my-4">
+      <div class="flex justify-center mb-12">
+        <div
+          class="w-2/3 py-12 px-4 border-x-2 border-b-2 border-t-4 mx-6 border-t-emerald-600 shadow-lg">
+          <div class="text-center">
+            <hr class=" border-black border-[1px] border-dashed mb-6 mx-10" />
 
-          <div>
-            <div>{data[0].vendor.vendorName}</div>
-            <div>{data[0].vendor.vendorLocation}</div>
-            <div>{data[0].vendor.postalCode}</div>
+            <div>
+              <div>{data[0].vendor.vendorName}</div>
+              <div>{data[0].vendor.vendorLocation}</div>
+              <div>{data[0].vendor.postalCode}</div>
+            </div>
+
+            <hr class="mt-6 mx-10 border-black border-[1px] border-dashed" />
           </div>
 
-          <hr class="mt-6 mx-10 border-black border-[1px] border-dashed" />
-        </div>
-
-        <div class="grid grid-cols-6 mt-12">
-          <div class="grid grid-cols-6 col-span-6 mb-6">
-            {#each data[0].items as item}
-              <div class="col-span-3">{item.itemName}</div>
-              <div class="col-span-3 text-right">{item.price}</div>
-            {/each}
+          <div class="grid grid-cols-6 mt-12">
+            <div class="grid grid-cols-6 col-span-6 mb-6">
+              {#each data[0].items as item}
+                <div class="col-span-3">{item.itemName}</div>
+                <div class="col-span-3 text-right">{item.price}</div>
+              {/each}
+            </div>
           </div>
-        </div>
 
-        <hr class="my-6 mx-10 border-black border-[1px] border-dashed" />
+          <hr class="my-6 mx-10 border-black border-[1px] border-dashed" />
 
-        <div class="grid grid-cols-6">
-          <div class="col-span-4">Subtotal</div>
-          <div class="col-span-2 text-right">{data[0].subtotal}</div>
+          <div class="grid grid-cols-6">
+            <div class="col-span-4">Subtotal</div>
+            <div class="col-span-2 text-right">{data[0].subtotal}</div>
 
-          <div class="col-span-4">GST</div>
-          <div class="col-span-2 text-right">{data[0].gst}</div>
+            <div class="col-span-4">GST</div>
+            <div class="col-span-2 text-right">{data[0].gst}</div>
 
-          <div class="col-span-4">Total</div>
-          <div class="col-span-2 text-right">{data[0].total}</div>
+            <div class="col-span-4">Total</div>
+            <div class="col-span-2 text-right">{data[0].total}</div>
 
-          <div class="col-span-4 text-left">{data[0].paymentMethod}</div>
-          <div class="col-span-2 text-right">{data[0].total}</div>
+            <div class="col-span-4 text-left">{data[0].paymentMethod}</div>
+            <div class="col-span-2 text-right">{data[0].total}</div>
 
-          <div class="col-span-4">Change</div>
-          <div class="col-span-2 text-right">{data[0].change}</div>
+            <div class="col-span-4">Change</div>
+            <div class="col-span-2 text-right">{data[0].change}</div>
 
-          <div class="col-span-2">Receipt ID</div>
-          <div class="col-span-4 text-right">{data[0].receiptId}</div>
-        </div>
-        <hr class="mt-6 mb-2 mx-10 border-black border-[1px] border-dashed" />
-        <div class="flex justify-center mb-2">
-          <div class="mr-4">{data[1]}</div>
-          <div class="mr-4">{data[2]}</div>
+            <div class="col-span-2">Receipt ID</div>
+            <div class="col-span-4 text-right">{data[0].receiptId}</div>
+          </div>
+          <hr class="mt-6 mb-2 mx-10 border-black border-[1px] border-dashed" />
+          <div class="flex justify-center mb-2">
+            <div class="mr-4">{data[1]}</div>
+            <div class="mr-4">{data[2]}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="font-normal text-xl text-center">
-      Manually Input Receipt:
-      <span class=" font-bold text-emerald-600">{data[0].receiptId}</span>
-      <hr class="border-t-2 border-black border-dashed my-2 mx-24" />
-      <button
-        on:click={() => {
-          downloadReceipt();
-        }}
-        class="text-sm mt-4 border-2 border-emerald-600 rounded-full w-2/3 py-3 transition hover:bg-emerald-600 hover:text-white"
-        on:mouseover={() => (isHovered = true)}
-        on:focus={() => (isHovered = true)}
-        on:blur={() => (isHovered = false)}
-        on:mouseover={() => (isHovered = true)}
-        on:focus={() => (isHovered = true)}
-        on:mouseout={() => (isHovered = false)}>
-        <svg
-          class="inline h-8 mr-2"
-          version="1.0"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1792.000000 2048.000000"
-          preserveAspectRatio="xMidYMid meet">
-          <g
-            transform="translate(0.000000,2048.000000) scale(0.100000,-0.100000)"
-            stroke="none">
-            <path
-              class:fill-white={isHovered}
-              d="M1710 16912 l0 -3567 -810 -810 -810 -810 0 -3697 0 -3698 810 0 810
+      <div class="font-normal text-xl text-center">
+        Manually Input Receipt:
+        <span class=" font-bold text-emerald-600">{data[0].receiptId}</span>
+        <hr class="border-t-2 border-black border-dashed my-2 mx-24" />
+        <button
+          on:click={() => {
+            downloadReceipt();
+          }}
+          class="text-sm mt-4 border-2 border-emerald-600 rounded-full w-2/3 py-3 transition hover:bg-emerald-600 hover:text-white"
+          on:mouseover={() => (isHovered = true)}
+          on:focus={() => (isHovered = true)}
+          on:blur={() => (isHovered = false)}
+          on:mouseover={() => (isHovered = true)}
+          on:focus={() => (isHovered = true)}
+          on:mouseout={() => (isHovered = false)}>
+          <svg
+            class="inline h-8 mr-2"
+            version="1.0"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1792.000000 2048.000000"
+            preserveAspectRatio="xMidYMid meet">
+            <g
+              transform="translate(0.000000,2048.000000) scale(0.100000,-0.100000)"
+              stroke="none">
+              <path
+                class:fill-white={isHovered}
+                d="M1710 16912 l0 -3567 -810 -810 -810 -810 0 -3697 0 -3698 810 0 810
         0 0 -2165 0 -2165 7255 0 7255 0 0 2165 0 2165 810 0 810 0 0 3703 0 3702
         -810 810 -810 810 0 1130 0 1130 -2433 2433 -2432 2432 -4823 0 -4822 0 0
         -3568z m9080 388 l0 -2240 2243 -2 2242 -3 3 -1662 2 -1663 -6310 0 -6310 0 0
@@ -205,19 +194,20 @@
         -945 0 0 -460 0 -460 810 0 810 0 0 -545 0 -545 -810 0 -810 0 0 -895 0 -895
         -650 0 -650 0 0 2445 0 2445 1595 0 1595 0 0 -545z m-50 -7170 l0 -1695 -6310
         0 -6310 0 0 1695 0 1695 6310 0 6310 0 0 -1695z" />
-            <path
-              class:fill-white={isHovered}
-              d="M4040 8679 l0 -641 343 4 c319 4 347 6 417 27 237 71 387 231 434
+              <path
+                class:fill-white={isHovered}
+                d="M4040 8679 l0 -641 343 4 c319 4 347 6 417 27 237 71 387 231 434
         463 20 99 21 292 1 376 -53 225 -207 345 -509 397 -66 11 -165 15 -387 15
         l-299 0 0 -641z" />
-            <path
-              class:fill-white={isHovered}
-              d="M8610 7905 l0 -1408 148 6 c345 15 577 86 760 233 263 210 392 523
+              <path
+                class:fill-white={isHovered}
+                d="M8610 7905 l0 -1408 148 6 c345 15 577 86 760 233 263 210 392 523
         422 1022 20 332 -20 688 -99 890 -58 147 -124 255 -215 351 -188 197 -449 290
         -868 308 l-148 6 0 -1408z" />
-          </g>
-        </svg>
-        Download as PDF</button>
+            </g>
+          </svg>
+          Download as PDF</button>
+      </div>
     </div>
-  </div>
-{/await}
+  {/await}
+</AuthGuard>
--- src/routes/(app)/receipt/[receipt_id]/+page.svelte
+++ src/routes/(app)/receipt/[receipt_id]/+page.svelte
@@ -1,17 +1,15 @@
 <script>
   import { firestore } from '$lib/firebase';
   import { goto } from '$app/navigation';
-  import { doc, getDoc, serverTimestamp, updateDoc } from 'firebase/firestore';
+  import { doc, serverTimestamp, updateDoc } from 'firebase/firestore';
   import { page } from '$app/stores';
   import { authStore } from '$lib/stores';
   import { getStorage, ref, getDownloadURL } from 'firebase/storage';
   import { AniIconLoading } from '$lib/icons';
   import dayjs from 'dayjs';
   import { getHttpsCallable } from '$lib/firebase/functions';
+  import { AuthGuard } from '$lib/components';
 
-  $: if ($authStore === null) {
-    goto('/account/signin');
-  }
   const userUid = $authStore?.uid;
   const storage = getStorage();
   let isHovered = false;
@@ -24,8 +22,7 @@
     const onReceiptViewCallable = getHttpsCallable('onReceiptViewCallable');
     const receiptData = await onReceiptViewCallable(current_receipt_id).catch(
       (err) => {
-        console.log(err);
-        goto('/');
+        console.error(err);
       }
     );
 
@@ -108,125 +105,126 @@
   let downloadLoading = false;
 </script>
 
-<div
-  class:hidden={!downloadLoading}
-  class="flex fixed justify-center items-center inset-0 w-full bg-black/50">
+<AuthGuard>
   <div
-    class="text-center text-md py-2 w-10/12 md:w-2/3 lg:w-1/3 xl:w-1/3 px-4 font-bold bg-white border-2">
-    Receipt PDF is Generating<br />
-    Wait a Moment...
-    <AniIconLoading class="m-auto my-4" fill="#059669" />
+    class:hidden={!downloadLoading}
+    class="flex fixed justify-center items-center inset-0 w-full bg-black/50">
+    <div
+      class="text-center text-md py-2 w-10/12 md:w-2/3 lg:w-1/3 xl:w-1/3 px-4 font-bold bg-white border-2">
+      Receipt PDF is Generating<br />
+      Wait a Moment...
+      <AniIconLoading class="m-auto my-4" fill="#059669" />
+    </div>
   </div>
-</div>
-<div class="px-4">
-  <div class="md:grid md:gap-8 md:grid-cols-2 md:grid-rows-3 my-4">
-    <div class=" border-t-4 border-emerald-600 bg-slate-400">
-      {#await promise}
-        <div class="bg-white h-full flex items-center justify-center">
-          <AniIconLoading fill="#059669" />
-        </div>
-      {:then receipt_data}
-        <div
-          class=" whitespace-normal m-auto col-span-2 md:col-span-1 bg-white py-6 shadow-lg">
-          <div class="text-center">
-            <!--Div for header-->
-            <div class="text-2xl font-bold my-2"
-              ><img
-                class="h-20 inline text-center"
-                src={receipt_data[1]}
-                alt="logo" /></div>
-            <div class="text-md font-light"
-              >{receipt_data[0]['vendor'].vendorName}</div>
-            <div class="text-md font-light"
-              >{receipt_data[0]['vendor'].vendorLocation}</div>
-            <div class="text-md font-light"
-              >Singapore {receipt_data[0]['vendor'].postalCode}</div>
-            <div class="text-left text-lg font-bold ml-6"
-              >Receipt ID: <span>{receipt_data[0]['receiptId']}</span></div>
+  <div class="px-4">
+    <div class="md:grid md:gap-8 md:grid-cols-2 md:grid-rows-3 my-4">
+      <div class=" border-t-4 border-emerald-600 bg-slate-400">
+        {#await promise}
+          <div class="bg-white h-full flex items-center justify-center">
+            <AniIconLoading fill="#059669" />
+          </div>
+        {:then receipt_data}
+          <div
+            class=" whitespace-normal m-auto col-span-2 md:col-span-1 bg-white py-6 shadow-lg">
+            <div class="text-center">
+              <!--Div for header-->
+              <div class="text-2xl font-bold my-2"
+                ><img
+                  class="h-20 inline text-center"
+                  src={receipt_data[1]}
+                  alt="logo" /></div>
+              <div class="text-md font-light"
+                >{receipt_data[0]['vendor'].vendorName}</div>
+              <div class="text-md font-light"
+                >{receipt_data[0]['vendor'].vendorLocation}</div>
+              <div class="text-md font-light"
+                >Singapore {receipt_data[0]['vendor'].postalCode}</div>
+              <div class="text-left text-lg font-bold ml-6"
+                >Receipt ID: <span>{receipt_data[0]['receiptId']}</span></div>
+              <div class=""
+                ><hr
+                  class="border-t-2 border-black border-dashed mb-2 mx-6" /></div>
+            </div>
+            <div class=" leading-7 grid grid-cols-6 mx-6 font-light">
+              {#each receipt_data[0]['items'] as item}
+                <div class="col-span-5">{item.itemName}</div>
+                <div class="col-span-1 text-right">${item.price}</div>
+              {/each}
+            </div>
             <div class=""
               ><hr
                 class="border-t-2 border-black border-dashed mb-2 mx-6" /></div>
+            <div class="grid grid-cols-6">
+              <!--receipt Totals div-->
+
+              <div class="col-span-1 mx-6 font-semibold">SUBTOTAL</div>
+              <div class="col-span-5 mx-6 text-right"
+                >${receipt_data[0]['subtotal']}</div>
+
+              <div class="col-span-1 mx-6 font-semibold"
+                >GST<span class="font-thin">(8%)</span></div>
+              <div class="col-span-5 mx-6 text-right"
+                >${receipt_data[0]['gst']}</div>
+
+              <div class="col-span-1 mx-6 font-semibold">TOTAL</div>
+              <div class="col-span-5 mx-6 text-right"
+                >${receipt_data[0]['total']}</div>
+
+              <div class="col-span-1 mx-6 font-semibold">CHANGE</div>
+              <div class="col-span-5 mx-6 text-right"
+                >${receipt_data[0]['change']}</div>
+
+              <div class="col-span-2 mx-6 font-semibold"
+                >{receipt_data[0]['paymentMethod']}</div>
+              <div class="col-span-4 mx-6 text-right"
+                >${receipt_data[0]['total']}</div>
+            </div>
+
+            <div class=""
+              ><hr
+                class="border-t-2 border-black border-dashed my-2 mx-6" /></div>
+            <div class="mx-6 grid grid-cols-6">
+              <!--footer div-->
+              <div class="text-center col-span-6"
+                ><span class="mr-2">{receipt_data[2]}</span>
+                <span class="mr-1">{receipt_data[3]}</span></div>
+            </div>
+            <div class="pl-6 font-bold text-emerald-600 text-left"
+              ><img
+                src="../ig-verified-regular.svg"
+                class="inline -mt-1"
+                alt="verified icon" />Dr. Receipts Verified
+            </div>
           </div>
-          <div class=" leading-7 grid grid-cols-6 mx-6 font-light">
-            {#each receipt_data[0]['items'] as item}
-              <div class="col-span-5">{item.itemName}</div>
-              <div class="col-span-1 text-right">${item.price}</div>
-            {/each}
-          </div>
-          <div class=""
-            ><hr
-              class="border-t-2 border-black border-dashed mb-2 mx-6" /></div>
-          <div class="grid grid-cols-6">
-            <!--receipt Totals div-->
+        {/await}
+      </div>
 
-            <div class="col-span-1 mx-6 font-semibold">SUBTOTAL</div>
-            <div class="col-span-5 mx-6 text-right"
-              >${receipt_data[0]['subtotal']}</div>
-
-            <div class="col-span-1 mx-6 font-semibold"
-              >GST<span class="font-thin">(8%)</span></div>
-            <div class="col-span-5 mx-6 text-right"
-              >${receipt_data[0]['gst']}</div>
-
-            <div class="col-span-1 mx-6 font-semibold">TOTAL</div>
-            <div class="col-span-5 mx-6 text-right"
-              >${receipt_data[0]['total']}</div>
-
-            <div class="col-span-1 mx-6 font-semibold">CHANGE</div>
-            <div class="col-span-5 mx-6 text-right"
-              >${receipt_data[0]['change']}</div>
-
-            <div class="col-span-2 mx-6 font-semibold"
-              >{receipt_data[0]['paymentMethod']}</div>
-            <div class="col-span-4 mx-6 text-right"
-              >${receipt_data[0]['total']}</div>
-          </div>
-
-          <div class=""
-            ><hr
-              class="border-t-2 border-black border-dashed my-2 mx-6" /></div>
-          <div class="mx-6 grid grid-cols-6">
-            <!--footer div-->
-            <div class="text-center col-span-6"
-              ><span class="mr-2">{receipt_data[2]}</span>
-              <span class="mr-1">{receipt_data[3]}</span></div>
-          </div>
-          <div class="pl-6 font-bold text-emerald-600 text-left"
-            ><img
-              src="../ig-verified-regular.svg"
-              class="inline -mt-1"
-              alt="verified icon" />Dr. Receipts Verified
-          </div>
-        </div>
-      {/await}
-    </div>
-
-    <div class="md:col-span-1">
-      {#if assigned}
-        <div class="my-6 text-center">
-          <button
-            on:click={() => {
-              downloadReceipt();
-            }}
-            class="border-2 border-emerald-600 rounded-full w-full py-3 transition hover:bg-emerald-600 hover:text-white"
-            on:mouseover={() => (isHovered = true)}
-            on:focus={() => (isHovered = true)}
-            on:blur={() => (isHovered = false)}
-            on:mouseover={() => (isHovered = true)}
-            on:focus={() => (isHovered = true)}
-            on:mouseout={() => (isHovered = false)}>
-            <svg
-              class="inline h-8 mr-2"
-              version="1.0"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1792.000000 2048.000000"
-              preserveAspectRatio="xMidYMid meet">
-              <g
-                transform="translate(0.000000,2048.000000) scale(0.100000,-0.100000)"
-                stroke="none">
-                <path
-                  class:fill-white={isHovered}
-                  d="M1710 16912 l0 -3567 -810 -810 -810 -810 0 -3697 0 -3698 810 0 810
+      <div class="md:col-span-1">
+        {#if assigned}
+          <div class="my-6 text-center">
+            <button
+              on:click={() => {
+                downloadReceipt();
+              }}
+              class="border-2 border-emerald-600 rounded-full w-full py-3 transition hover:bg-emerald-600 hover:text-white"
+              on:mouseover={() => (isHovered = true)}
+              on:focus={() => (isHovered = true)}
+              on:blur={() => (isHovered = false)}
+              on:mouseover={() => (isHovered = true)}
+              on:focus={() => (isHovered = true)}
+              on:mouseout={() => (isHovered = false)}>
+              <svg
+                class="inline h-8 mr-2"
+                version="1.0"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1792.000000 2048.000000"
+                preserveAspectRatio="xMidYMid meet">
+                <g
+                  transform="translate(0.000000,2048.000000) scale(0.100000,-0.100000)"
+                  stroke="none">
+                  <path
+                    class:fill-white={isHovered}
+                    d="M1710 16912 l0 -3567 -810 -810 -810 -810 0 -3697 0 -3698 810 0 810
         0 0 -2165 0 -2165 7255 0 7255 0 0 2165 0 2165 810 0 810 0 0 3703 0 3702
         -810 810 -810 810 0 1130 0 1130 -2433 2433 -2432 2432 -4823 0 -4822 0 0
         -3568z m9080 388 l0 -2240 2243 -2 2242 -3 3 -1662 2 -1663 -6310 0 -6310 0 0
@@ -241,45 +239,46 @@
         -945 0 0 -460 0 -460 810 0 810 0 0 -545 0 -545 -810 0 -810 0 0 -895 0 -895
         -650 0 -650 0 0 2445 0 2445 1595 0 1595 0 0 -545z m-50 -7170 l0 -1695 -6310
         0 -6310 0 0 1695 0 1695 6310 0 6310 0 0 -1695z" />
-                <path
-                  class:fill-white={isHovered}
-                  d="M4040 8679 l0 -641 343 4 c319 4 347 6 417 27 237 71 387 231 434
+                  <path
+                    class:fill-white={isHovered}
+                    d="M4040 8679 l0 -641 343 4 c319 4 347 6 417 27 237 71 387 231 434
         463 20 99 21 292 1 376 -53 225 -207 345 -509 397 -66 11 -165 15 -387 15
         l-299 0 0 -641z" />
-                <path
-                  class:fill-white={isHovered}
-                  d="M8610 7905 l0 -1408 148 6 c345 15 577 86 760 233 263 210 392 523
+                  <path
+                    class:fill-white={isHovered}
+                    d="M8610 7905 l0 -1408 148 6 c345 15 577 86 760 233 263 210 392 523
         422 1022 20 332 -20 688 -99 890 -58 147 -124 255 -215 351 -188 197 -449 290
         -868 308 l-148 6 0 -1408z" />
-              </g>
-            </svg>
-            Download as PDF</button>
-        </div>
-      {/if}
-      <div class="md:h-28 md:my-0 my-3 border-2 px-3 py-3 shadow rounded-lg">
-        <div class:hidden={!assigned}>
-          <div class="text-center">Have troubles with this receipt?</div>
-          <div class="text-center"
-            ><button
-              on:click={() => {
-                goto('/disputeHandling');
-              }}
-              class="md:mt-3 border-2 bg-emerald-600 px-5 py-2.5 m-2 text-white rounded-lg text-center"
-              >File Disputes with Vendor</button
-            ></div>
-        </div>
-        <div class:hidden={assigned}>
-          <div class="text-center">Want to assign receipt to account?</div>
-          <div class="text-center"
-            ><button
-              on:click={() => {
-                assignReceipt();
-              }}
-              class="md:mt-3 border-2 bg-emerald-600 px-5 py-2.5 m-2 text-white rounded-lg text-center"
-              >Assign to Account</button
-            ></div>
+                </g>
+              </svg>
+              Download as PDF</button>
+          </div>
+        {/if}
+        <div class="md:h-28 md:my-0 my-3 border-2 px-3 py-3 shadow rounded-lg">
+          <div class:hidden={!assigned}>
+            <div class="text-center">Have troubles with this receipt?</div>
+            <div class="text-center"
+              ><button
+                on:click={() => {
+                  goto('/disputeHandling');
+                }}
+                class="md:mt-3 border-2 bg-emerald-600 px-5 py-2.5 m-2 text-white rounded-lg text-center"
+                >File Disputes with Vendor</button
+              ></div>
+          </div>
+          <div class:hidden={assigned}>
+            <div class="text-center">Want to assign receipt to account?</div>
+            <div class="text-center"
+              ><button
+                on:click={() => {
+                  assignReceipt();
+                }}
+                class="md:mt-3 border-2 bg-emerald-600 px-5 py-2.5 m-2 text-white rounded-lg text-center"
+                >Assign to Account</button
+              ></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+</AuthGuard>
